069f3482789e35814fc63b4f3db41612cc02463d
Eduardo Lima <ce-lima@hotmail.com>
2022-11-30 11:30:21 -0300

    Extract CRU v2

--- Extract_CRU.py
+++ Extract_CRU.py
@@ -1,35 +1,39 @@
 #%%
-import rioxarray as rxr
-import xarray as xr
-import numpy as np
 import pandas as pd
-import os
 import geopandas as gpd
-import matplotlib.pyplot as plt
-import pyproj
-from glob import glob
+import os
+import numpy as np
+import xarray as xr
 import netCDF4 as nc
-import scipy as sc
-from shapely.geometry import point
-#%%
-basins = ["Redencao_WGS84"]
+import matplotlib.pyplot as plt
+from glob import glob
 
-list_nc = glob("{}/Data/*.nc".format(os.getcwd()))
+list_nc = glob("CRU/*.nc")
 
-for basin in basins:
+for dir_nc in list_nc:
+    
+    var = dir_nc.split(".")[-3]
+    nc_data = xr.open_dataset(dir_nc)
 
-    dir_shp = "{}/Shapes/{}.shp".format(os.getcwd(), basin)
-    shp = gpd.read_file(dir_shp)
+    lon, lat = np.meshgrid(nc_data["lon"], nc_data["lat"])
+    df_point = pd.DataFrame({"lon": lon.flatten(), "lat": lat.flatten()})
+    shp_point = gpd.GeoDataFrame(df_point, geometry = gpd.points_from_xy(df_point["lon"], df_point["lat"], crs="EPSG:4326"))
 
-    for path_nc in list_nc:
+    basins = glob("shapes/*.shp")
 
-        ds = rxr.open_rasterio(path_nc, engine = "netcdf4", masked=True)
-        ds.rio.write_crs("EPSG:4326", inplace = True)
-        name = path_nc.split(".")[5]
+    for basin in basins:
+        shp_basin = gpd.read_file(basin)
 
-        ds_clip = ds.rio.clip(shp['geometry'], all_touched = True)
-        df = ds_clip.to_dataframe().reset_index()
-        #df.drop(["spatial_ref", "stn"], axis = 1, inplace = True)
+        gdf_result = gpd.sjoin(shp_point, shp_basin, how = "left")
+        gdf_result = gdf_result.loc[~np.isnan(gdf_result["index_right"])]
+        ins_point = gdf_result[["lon", "lat"]].values[:]
+        ins_df = pd.DataFrame(ins_point, columns = ["lon", "lat"])
 
-        df.to_csv("{}/Extracted/CRU_{}.csv".format(os.getcwd(), name), sep = ";", index = False)
+        var_ins = []
+
+        for i in range(len(ins_df)):
+            var_ins.append(nc_data[var].sel(lat = ins_df["lat"][i], lon = ins_df["lon"][i]).values)
+
+        var_df = ins_df[["lon", "lat"]].join(pd.DataFrame(var_ins, columns = nc_data["time"]))
+        var_df.to_csv("Extracted/{}_{}.csv".format(basin.split("\\")[1].split(".")[0], var), sep = ";", index = None, header = True)
 #%%
